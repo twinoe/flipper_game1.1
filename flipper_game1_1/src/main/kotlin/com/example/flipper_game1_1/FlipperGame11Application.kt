4c5d28a50316f82527a2e522c294dd782a41c7ca
ÖmerEroglu <oemer.eroglu@mail.schwarz>
2025-03-31 10:43:26 +0200

    project setup

---
+++ flipper_game1_1/src/main/kotlin/com/example/flipper_game1_1/FlipperGame11Application.kt
@@ -0,0 +1,11 @@
+package com.example.flipper_game1_1
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FlipperGame11Application
+
+fun main(args: Array<String>) {
+    runApplication<FlipperGame11Application>(*args)
+}
---
+++ flipper_game1_1/src/test/kotlin/com/example/flipper_game1_1/FlipperGame11ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.flipper_game1_1
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FlipperGame11ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
