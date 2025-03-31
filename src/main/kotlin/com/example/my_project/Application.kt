8eed8b8366e7ffb4d7c0f62c7abfe5f170bd0be2
ÖmerEroglu <oemer.eroglu@mail.schwarz>
2025-03-31 11:21:23 +0200

    setup3.5

---
+++ src/main/kotlin/com/example/my_project/Application.kt
@@ -0,0 +1,12 @@
+package com.example.my_project
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Application
+
+fun main(args: Array<String>) {
+    runApplication<Application>(*args)
+    println("Hello World!")
+}
